2cba1dadf3f21f672318123c1372a00dd393bd60
mranderson0193 <sudhanshu0193@gmail.com>
2026-04-16 20:37:51 -0400

    This is my update

--- calculatoronline.py
+++ calculatoronline.py
@@ -13,7 +13,10 @@ def calculate():
         expression = request.form['expression']
         # Securely evaluate expressions using a limited dictionary of allowed functions
         # This prevents arbitrary code execution vulnerabilities with eval()
-        safe_dict = {'math': math, '__builtins__': {}}
+        safe_dict = {'math': math, '__builtins__': {}, 
+                     'sin': math.sin, 'cos': math.cos, 'tan': math.tan, 
+                     'log': math.log10, 'sqrt': math.sqrt, 'exp': math.exp,
+                     'pi': math.pi, 'e': math.e}
         result = str(eval(expression, safe_dict))
         return render_template('index.html', result=result, expression=expression)
     except Exception as e:
